4b1d8d6ebe7a7f6095518ee0931100ca35cdfaf3
Sujal Shresth <sujshrest@example.com>
2024-02-27 15:58:03 +0530

    Installed mongoose, dotenv and added MongoDB client code

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+const {
+  connectDB,
+  disconnectDB,
+  isConnected,
+} = require("./config/dbConnection");
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get("/ping", (req, res) => {
-  res.send("Pong");
+app.get("/", (req, res) => {
+  if (isConnected()) {
+    res.send("📦 connected to mongoDB");
+  } else {
+    res.send("❌ error connecting to mongoDB");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening at port: ${port}`);
+  connectDB();
 });
