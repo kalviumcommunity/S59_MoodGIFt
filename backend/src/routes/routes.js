90d13f16e66aab067f68644565021b72403e41d2
Sujal Shresth <sujshrest@example.com>
2024-03-11 15:06:34 +0530

    Added user model and joi validation for memes and user data

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
+
+// import connectDB function to initiate connection
 const { connectDB } = require("../connection/dbConnection");
+
+// import createMemeModel to dynamically create the model
 const { createMemeModel } = require("../models/memeModel");
 
+const { User } = require("../models/userModel");
 connectDB();
 
+// joi meme schema for validation
+const memeSchema = Joi.object({
+  name: Joi.string().required(),
+  url: Joi.string().uri().required(),
+  mood_category: Joi.string().required(),
+});
+
+// joi meme schema for patch request validation
+const patchMemeSchema = Joi.object({
+  name: Joi.string(),
+  url: Joi.string().uri(),
+  mood_category: Joi.string(),
+}).or("name", "url", "mood_category");
+
+// joi schema for user validation
+const userSchema = Joi.object({
+  fullname: Joi.string().required(),
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+// get endpoint for api
 router.get("/:category", async (req, res) => {
   try {
     const category = req.params.category + "_meme";
@@ -19,11 +48,17 @@ router.get("/:category", async (req, res) => {
   }
 });
 
+// post meme endpoint for posting memes
 router.post("/postMeme/:category", async (req, res) => {
   try {
     const category = req.params.category + "_meme";
 
     let memeModel = createMemeModel(category);
+
+    const { error } = memeSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(400).json({ error: error.details });
+    }
 
     const newMeme = new memeModel(req.body);
     const savedNewMeme = await newMeme.save();
@@ -34,11 +69,56 @@ router.post("/postMeme/:category", async (req, res) => {
   }
 });
 
+// post end point for creating new user
+router.post("/register", async (req, res) => {
+  try {
+    const { error } = userSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res.status(400).json({ error: error.details });
+    }
+
+    const { fullname, username, email, password } = req.body;
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already registered" });
+    }
+
+    const newUser = new User({
+      fullname,
+      username,
+      email,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: savedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// patch endpoint
 router.patch("/patch/:category/:id", async (req, res) => {
   try {
     const category = req.params.category + "_meme";
 
     let memeModel = createMemeModel(category);
+
+    const { error } = patchMemeSchema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      return res.status(400).json({ error: error.details });
+    }
 
     const updateStatus = await memeModel.findByIdAndUpdate(
       req.params.id,
@@ -56,11 +136,20 @@ router.patch("/patch/:category/:id", async (req, res) => {
   }
 });
 
+// put endpoint
 router.put("/replace/:category/:id", async (req, res) => {
   try {
-    let AngryMemeModel = createMemeModel("Angry");
+    const category = req.params.category + "_meme";
 
-    const updateStatus = await AngryMemeModel.findByIdAndUpdate(
+    let memeModel = createMemeModel(category);
+
+    const { error } = memeSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    const updateStatus = await memeModel.findByIdAndUpdate(
       req.params.id,
       req.body,
       { new: true }
@@ -76,6 +165,7 @@ router.put("/replace/:category/:id", async (req, res) => {
   }
 });
 
+// delete endpoint
 router.delete("/delete/:category/:id", async (req, res) => {
   try {
     const category = req.params.category + "_meme";
